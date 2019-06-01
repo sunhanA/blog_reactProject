bccb9e2604233b648010f665a8fe6a0af42ea272
295714865 <295714865@qq.com>
2019-06-01 21:06:58 +0800

    首页基本功能补充

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, {Component} from "react";
 import "./App.css";
 
 class App extends Component{
+    state = {
+        isShow: true,
+    }
     render(){
         return (
             <div className="App">
@@ -9,6 +12,7 @@ class App extends Component{
                     <span className = 'circlePart circleLeft'>S</span>
                     <span className = 'circlePart circleRight'>H</span>
                 </div>
+
                 <div className = 'moveImgBlock'>
                     <img 
                         className = 'moveImg' 
@@ -19,30 +23,52 @@ class App extends Component{
                         onMouseLeave = {(e) => this.recover(e)}
                     />
                 </div>
+
                 <div className = 'footer'>
                     <ul>
                         <li>
                             <a href = 'http://sunh.top' className = 'small' draggable = {false}>首页</a>
-                            <img src = 'images/huaji.png' className = 'menuIcon small' draggable = {false} alt = '菜单'/>
+                            <div className = 'menuIconBg small'>
+                                <img className = 'smallImg' src = 'images/notebook.svg'  draggable = {false} alt = '菜单'/>
+                            </div>
                         </li>
                         <li>
                             <a href = 'http://sunh.top' className = 'middle' draggable = {false}>首页</a>
-                            <img src = 'images/huaji.png' className = 'menuIcon middle' draggable = {false} alt = '菜单'/>
-                            </li>
+                            <div className = 'menuIconBg middle'>
+                                <img className = 'middleImg' src = 'images/bank.svg'  draggable = {false} alt = '菜单'/>
+                            </div>
+                        </li>
                         <li>
                             <a href = 'http://sunh.top' className = 'large' draggable = {false}>首页</a>
-                            <img src = 'images/huaji.png' className = 'menuIcon large' draggable = {false} alt = '菜单'/>
+                            <div className = 'menuIconBg large'>
+                                <img className = 'largeImg' src = 'images/files-and-folders.svg'  draggable = {false} alt = '菜单'/>
+                            </div>
                         </li>
                         <li>
                             <a href = 'http://sunh.top' className = 'middle' draggable = {false}>首页</a>
-                            <img src = 'images/huaji.png' className = 'menuIcon middle' draggable = {false} alt = '菜单'/>
+                            <div className = 'menuIconBg middle'>
+                                <img className = 'middleImg' src = 'images/app.svg'  draggable = {false} alt = '菜单'/>
+                            </div>
                         </li>
                         <li>
                             <a href = 'http://sunh.top' className = 'small' draggable = {false}>首页</a>
-                            <img src = 'images/huaji.png' className = 'menuIcon small' draggable = {false} alt = '菜单'/>
+                            <div className = 'menuIconBg small'>
+                                <img className='smallImg' src = 'images/paper-plane.svg'  draggable = {false} alt = '菜单'/>
+                            </div>
                         </li>
                     </ul>
                     <p className = 'copyRightText'>Design by sunh个人主页 鄂ICP备18007225</p>
+                </div>
+
+                <div className = 'switchBtn' onClick = {() => this.controlShow()}>{this.state.isShow ? '关闭弹幕' : '打开弹幕'}</div>
+
+                <div className = 'share'>
+                    <p className = 'shareTitle'>分享</p>
+                    <ul className = 'shareList' onClick = {(e) => this.share(e)}>
+                        <li><img src = '/images/qq.svg' alt = 'qq' draggable = {false} /><span>分享给好友</span></li>
+                        <li><img src = '/images/qqzone.svg' alt = 'qqzone' draggable = {false}/><span>分享到空间</span></li>
+                        <li><img src = '/images/sina.svg' alt = 'sina' draggable = {false}/><span>分享到微博</span></li>
+                    </ul>
                 </div>
             </div>
         );
@@ -89,6 +115,50 @@ class App extends Component{
     
     recover(e){
         e.target.style = `transform: translate3d(0px, 0px, 0px) skew(0deg)`;
+    }
+
+    controlShow(){
+        this.setState({isShow: !this.state.isShow});
+    }
+
+    share(e){
+        if(e.target.nodeName === 'IMG'){
+            return;
+        }
+        let shareText = e.target.innerHTML;
+        let p = {
+            /*获取URL，可加上来自分享到QQ标识，方便统计*/  
+            url: 'http://sunh.top',   
+            desc: '查看他的个人主页http://sunh.top',
+            /*分享标题(可选)*/
+            title: '分享孙寒的个人主页',
+            /*分享摘要(可选)*/
+            summary: '还不赶紧点击进来观看',
+            /*分享图片(可选)*/
+            pics: '',
+            /*视频地址(可选)*/
+            flash: '',
+           /*分享来源(可选) 如：QQ分享*/
+           site: '',
+           style: '',
+           width: 32,
+           height: 32
+        };
+        let content = [];
+        let shareUrl;
+        for(let pro in p){
+            content.push(`${pro}=${p[pro]}`);
+        }
+        if(shareText === '分享给好友'){
+            shareUrl = 'http://connect.qq.com/widget/shareqq/index.html?';
+        }
+        else if (shareText === '分享到空间'){
+            shareUrl = 'http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?';
+        }
+        else if (shareText === '分享到微博'){
+            shareUrl = 'http://service.weibo.com/share/share.php?';
+        }
+        window.open(shareUrl+content.join('&'));
     }
 }
 export default App;
